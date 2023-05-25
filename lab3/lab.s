93a513d422da0ef151ba0ec4bfe52e847172ccb9
Bob Nicholson <bobniklv@gmail.com>
2023-05-25 23:18:02 +0300

    - Updated lab3

--- lab3/lab.s
+++ lab3/lab.s
@@ -20,7 +20,7 @@ section .data
     err_too_many_argv db "Error: too many arguments. Please, use ./lab <filename> to run program properly", 0x0a, 0
 
 ; SIZES
-    buffer_size dq 15
+    buffer_size dq 10
     output_size dq 0
 
 ; FILE
