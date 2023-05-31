22db0507249984cf5cbb041731bc1df214afab8e
Bob Nicholson <bobniklv@gmail.com>
2023-05-31 17:38:34 +0300

    - Updated lab3

--- lab3/lab.s
+++ lab3/lab.s
@@ -251,7 +251,7 @@ put_word_into_output_buffer:
         .loop: 
             cmp     r9, 0
             je      .end
-            mov     si, byte [word_pointer]
+            mov     esi, byte [word_pointer]
             inc     rsi
             inc     qword [word_pointer]
             dec     r9
