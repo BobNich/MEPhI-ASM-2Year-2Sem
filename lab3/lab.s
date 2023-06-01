f033d0595466287e6784f89375f73cbabb089244
Bob Nicholson <bobniklv@gmail.com>
2023-06-01 21:06:22 +0300

    - Updated lab3

--- lab3/lab.s
+++ lab3/lab.s
@@ -238,7 +238,9 @@ put_word_into_output_buffer:
         mov     byte [first_word_completed], TRUE
         je      .add_word
         .add_space:
+            push    r12
             mov     byte [rsi + r12], SPACE
+            push    r12
             inc     r12
         .add_word:
             xor     rcx, rcx
