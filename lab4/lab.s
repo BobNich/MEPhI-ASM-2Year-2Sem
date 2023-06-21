75b334cf8e5829c496261d8fd8d4d8cbd2876753
Bob Nicholson <bobniklv@gmail.com>
2023-06-21 20:29:29 +0300

    - Updated lab4

--- lab4/lab.s
+++ lab4/lab.s
@@ -9,7 +9,7 @@ section .data
     aArgsError      db "Use ./lab <filename> to run program properly", 0x0a, 0
     aFileOpenFailed db 'Error: File open failed.',0Ah,0
     aTermInfinity   db 'Term is infinity',0Ah,0
-    aSeriesMember   db "%-10d",0x0a,0
+    aSeriesMember   db "%-10d %f",0x0a,0
     aInputPrecision db 'Input precision: ',0
     aLibResultF     db 'Lib result: %f',0Ah,0
     aCustomResultF  db 'Custom result: %f',0Ah,0
