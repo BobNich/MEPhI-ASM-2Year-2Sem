7eb1470d8a060cd0476d25f0ad6c8b3c60a14497
Bob Nicholson <bobniklv@gmail.com>
2023-06-22 21:16:59 +0300

    - Updated lab5

--- lab5/main.c
+++ lab5/main.c
@@ -4,7 +4,7 @@ extern void process_image(char *input_filename, char *output_filename);
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <input file> <output file>\n", argv[0]);
+        printf("Please, use: %s $input_file$ $output_file$\n", argv[0]);
         return 1;
     }
 
