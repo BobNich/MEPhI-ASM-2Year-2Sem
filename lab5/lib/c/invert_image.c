709130f51cbc69971408f327d4d7a0340343ca72
Bob Nicholson <bobniklv@gmail.com>
2023-06-25 14:52:10 +0300

    - Updated lab5

--- lab5/lib/c/invert_image.c
+++ lab5/lib/c/invert_image.c
@@ -2,33 +2,47 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "stb_image.h"
 #include "stb_image_write.h"
 
 int invert_image(char *input_filename, char *output_filename) {
-    FILE *input = fopen(input_filename, "rb");
-    if (input == NULL) {
-        perror("Error opening input file");
-        return 1;
-    }
-
     int width, height, channels;
-    unsigned char *image = stbi_load_from_file(input, &width, &height, &channels, 0);
-
-    if (image == NULL) {
-        printf("Could not load image\n");
-        return 1;
+    unsigned char *image_data = stbi_load(input_filename, &width, &height, &channels, 0);
+    if (!image_data) {
+        printf("Error loading image: %s\n", stbi_failure_reason());
+        return 0;
     }
 
-    unsigned char *inverted_image = (unsigned char *) malloc(width * height * channels);
-
-    for (int i = 0; i < width * height * channels; i++) {
-        inverted_image[i] = image[i];
+    if (channels != 3) {
+        printf("Error: Only 24-bit BMP images are supported.\n");
+        stbi_image_free(image_data);
+        return 0;
     }
 
-    stbi_write_bmp(output_filename, width, height, channels, inverted_image);
-    stbi_image_free(image);
-    free(inverted_image);
+    unsigned char *inverted_data = (unsigned char *)malloc(width * height * channels);
+    if (!inverted_data) {
+        printf("Error allocating memory for inverted image.\n");
+        stbi_image_free(image_data);
+        return 0;
+    }
 
-    return 0;
+    // Invert the image
+    int pixel_count = width * height;
+    for (int i = 0; i < pixel_count; i++) {
+        inverted_data[(i * channels) + 0] = 255 - image_data[(i * channels) + 0]; // Red channel
+        inverted_data[(i * channels) + 1] = 255 - image_data[(i * channels) + 1]; // Green channel
+        inverted_data[(i * channels) + 2] = 255 - image_data[(i * channels) + 2]; // Blue channel
+    }
+
+    if (!stbi_write_bmp(output_filename, width, height, channels, inverted_data)) {
+        printf("Error writing output image: %s\n", stbi_failure_reason());
+        free(inverted_data);
+        stbi_image_free(image_data);
+        return 0;
+    }
+
+    free(inverted_data);
+    stbi_image_free(image_data);
+    return 1;
 }
