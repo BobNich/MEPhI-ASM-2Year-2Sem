4324c980356f4b9c6f3311153ec9746b5b9c3645
Bob Nicholson <bobniklv@gmail.com>
2023-06-29 12:25:12 +0300

    - Updated lab5

--- lab5/lib/c/invert_image.c
+++ lab5/lib/c/invert_image.c
@@ -4,12 +4,8 @@
 void invert_image(unsigned char *image_data, int width, int height, int channels) {
     int pixel_count = width * height;
     for (int i = 0; i < pixel_count; i++) {
-        int x = i % width;
-        int y = i / height;
-        if (x >= y) {
-            image_data[(i * channels) + 0] = image_data[(i * channels) + 0] ^ 0xff;
-            image_data[(i * channels) + 1] = image_data[(i * channels) + 1] ^ 0xff;
-            image_data[(i * channels) + 2] = image_data[(i * channels) + 2] ^ 0xff; 
-        }
+        image_data[(i * channels) + 0] = image_data[(i * channels) + 0] ^ 0xff;
+        image_data[(i * channels) + 1] = image_data[(i * channels) + 1] ^ 0xff;
+        image_data[(i * channels) + 2] = image_data[(i * channels) + 2] ^ 0xff; 
     }
 }
